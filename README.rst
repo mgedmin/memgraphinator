7cdfafd7fd1a7083558a035691eaebb6cb6933a3
Marius Gedminas <marius@gedmin.as>
2014-09-24 13:00:04 +0300

    Implement process selection

--- README.rst
+++ README.rst
@@ -13,7 +13,7 @@ to see more or less.
 
 Usage::
 
-    ./memgraphinator.py pid
+    ./memgraphinator.py [pid]
 
 
 Requirements
@@ -32,5 +32,5 @@ Future plans
 - Show current and peak memory usage numbers below the graph
 - Show process name and command-line arguments above the graph
 - Changeable zoom level (1px = 0.1s, 1s, 10s, entire graph)
-- Process picker in the GUI
+- Searching/filtering in the process picker
 - Ability to watch multiple processes simultaneously
--- memgraphinator.py
+++ memgraphinator.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python
+import signal
 import sys
+import os
 
 import gi
 gi.require_version('Gtk', '3.0')
-from gi.repository import GObject, Gtk
+from gi.repository import GObject, Gtk, Pango
 
 
 if len(sys.argv) < 2:
-    sys.exit('Usage: %s pid' % sys.argv[0])
-
-pid = int(sys.argv[1])
+    pid = None
+else:
+    pid = int(sys.argv[1])
 
 
 def mem_usage(pid):
@@ -85,17 +87,46 @@ class Graph(Gtk.DrawingArea):
 
 
 class MainWindow(Gtk.Window):
+
+    _pid = None
+
     def __init__(self, pid):
         super(MainWindow, self).__init__()
-        self.pid = pid
-
-        self.set_default_size(300, 100)
-
-        self.graph = Graph()
-        self.add(self.graph)
-        self.set_property("title", "Memory usage of %d" % pid)
 
         self.connect("delete-event", Gtk.main_quit)
+        self.set_default_size(300, 100)
+        self.set_border_width(6)
+
+        self.select_button = Gtk.Button("Select a process")
+        self.select_button.set_relief(Gtk.ReliefStyle.NONE)
+        self.select_button.connect("clicked", self.select_process)
+
+        self.graph = Graph()
+
+        self.pid = pid
+
+    @property
+    def pid(self):
+        return self._pid
+
+    @pid.setter
+    def pid(self, new_pid):
+        child = self.get_child()
+        if child:
+            self.remove(child)
+        if new_pid:
+            self._pid = new_pid
+            self.set_title("Memory usage of %d" % new_pid)
+            self.add(self.graph)
+            self.graph.show()
+            self._start_polling()
+        else:
+            self.set_title("Memory usage of a process")
+            self.add(self.select_button)
+            self.select_button.show()
+
+    def _start_polling(self):
+        self._start_polling = lambda: None  # don't do this again
         self.poll()
         GObject.timeout_add(100, self.poll)
 
@@ -103,7 +134,75 @@ class MainWindow(Gtk.Window):
         self.graph.add_point(mem_usage(self.pid))
         return True
 
+    def select_process(self, target):
+        process_selector_dialog = ProcessSelector(self)
+        if process_selector_dialog.run() == Gtk.ResponseType.OK:
+            self.pid = process_selector_dialog.pid
+        process_selector_dialog.destroy()
+
+
+class ProcessSelector(Gtk.Dialog):
+
+    def __init__(self, parent):
+        super(ProcessSelector, self).__init__(
+            "Select a process", parent=parent,
+            flags=Gtk.DialogFlags.MODAL | Gtk.DialogFlags.USE_HEADER_BAR,
+            buttons=(Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL,
+                     Gtk.STOCK_OK, Gtk.ResponseType.OK))
+        self.set_default_size(600, 400)
+        area = self.get_content_area()
+        self.store = Gtk.ListStore(int, str, int, str) # pid, command, size, size_string
+        self.refresh_process_list()
+        self.process_list = Gtk.TreeView(model=self.store)
+        self.process_list.set_search_column(1)
+        column = Gtk.TreeViewColumn("PID", Gtk.CellRendererText(xalign=1.0), text=0)
+        column.set_sort_column_id(0)
+        self.process_list.append_column(column)
+        column = Gtk.TreeViewColumn("Command", Gtk.CellRendererText(), text=1)
+        column.set_sort_column_id(1)
+        column.set_expand(True)
+        column.set_max_width(200)
+        self.process_list.append_column(column)
+        column = Gtk.TreeViewColumn("Size", Gtk.CellRendererText(xalign=1.0), text=3)
+        column.set_sort_column_id(2)
+        self.process_list.append_column(column)
+        self.process_list.connect("row_activated", self.select_process)
+        w = Gtk.ScrolledWindow()
+        w.set_policy(Gtk.PolicyType.AUTOMATIC, Gtk.PolicyType.AUTOMATIC)
+        w.add(self.process_list)
+        area.pack_start(w, True, True, 0)
+        area.show_all()
+
+    @property
+    def pid(self):
+        model, iter = self.process_list.get_selection().get_selected()
+        if not iter:
+            return None
+        return model[iter][0]
+
+    def select_process(self, treeview, path, view_column):
+        self.response(Gtk.ResponseType.OK)
+
+    def refresh_process_list(self):
+        self.store.clear()
+        for n in os.listdir('/proc'):
+            if n.isdigit():
+                try:
+                    with open('/proc/%s/cmdline' % n, 'rb') as f:
+                        cmdline = f.read().replace('\0', ' ')
+                    if not cmdline:
+                        with open('/proc/%s/stat' % n, 'rb') as f:
+                            stat = f.read()
+                            cmdline = stat.partition('(')[-1].rpartition(')')[0]
+                except IOError:
+                    continue
+                pid = int(n)
+                size = mem_usage(pid) or 0
+                size_mb = '{:,} MB'.format(size / 1024)
+                self.store.append([pid, cmdline, size, size_mb])
+
 
 win = MainWindow(pid)
 win.show_all()
+signal.signal(signal.SIGINT, signal.SIG_DFL)
 Gtk.main()
